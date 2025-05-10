8f9fb1b52558f75d6ee748c18b332712c76c797f
xiaozhizz <85253608+xiaozhizz@users.noreply.github.com>
2025-05-11 04:34:21 +0800

    创建 suijibizhi.js

---
+++ suijibizhi.js
@@ -0,0 +1,9 @@
+
+$notification.post("🌞","☁️","随机壁纸")
+
+var attach = {
+    "openUrl":"https://bing.ee123.net/img/rand",
+    "mediaUrl":"https://photo.16pic.com/00/04/35/16pic_435715_b.jpg",
+    "clipboard":"tap to copy",
+}
+$notification.post("🌞","☁️","点击查看",attach)
